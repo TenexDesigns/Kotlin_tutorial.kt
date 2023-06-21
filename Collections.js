9ae78db8f69c071bb34eceab207e2e154cbc240f
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-06-21 11:48:12 +0300

    Update Collections.js

--- Collections.js
+++ Collections.js
@@ -140,28 +140,97 @@ mutableSet.remove("banana") // Remove an element
 IMMUTABLE COLLECTIONS
 _________________________________________________________________________________________________________________________
 
-4.
+
+
+4.Immutable Lists:
+Immutable lists are collections with read-only access, and their elements cannot be modified.
+
+Unique Features:
+Immutable lists provide a fixed set of elements that cannot be changed.
+
+Methods of Immutable List:
+          listOf(): Creates an immutable list with the given elements.
+          listOf<T>(): Creates an empty immutable list.
+
+Some common methods:
+          get(index: Int): Retrieves the element at the specified index.
+          indexOf(element: T): Returns the index of the first occurrence of the specified element, or -1 if not found.
+          subList(fromIndex: Int, toIndex: Int): Returns a view of the portion of the list between the specified indices.
+
+EXAMPLE
+
+val immutableList = listOf("apple", "banana", "orange")
+val element = immutableList[0] // Access an element
+val immutableList = listOf(1, 2, 3)
+val element = immutableList[1]
+val index = immutableList.indexOf(3)
+val subList = immutableList.subList(1, 3)
 
 
 
 
 
+5.Immutable Maps:
+
+Immutable maps are collections that store key-value pairs and cannot be modified.
+
+Unique Features:
+Immutable maps provide a fixed set of key-value pairs that cannot be changed.
+
+Methods of Immutable Map:
+          mapOf(): Creates an immutable map with the given key-value pairs.
+          
+          
+Some common methods:
+          get(key: K): Retrieves the value corresponding to the specified key, or null if not found.
+          keys: Returns a read-only set of the maps keys.
+          values: Returns a read-only collection of the maps values.
+
+EXAMPLE
+val immutableMap = mapOf("name" to "John", "age" to 25)
+val value = immutableMap["name"] // Access a value by key
+          
+val immutableMap = mapOf("one" to 1, "two" to 2)
+val value = immutableMap["one"]
+val keys = immutableMap.keys
+val values = immutableMap.values
 
 
 
 
 
+6.Immutable Sets:
+
+Immutable sets are collections that store unique elements and cannot be modified.
+
+Unique Features: 
+Immutable sets provide a fixed set of unique elements that cannot be changed.
+
+Methods of Immutable Set:
+          setOf(): Creates an immutable set with the given elements.
+          
+Some common methods:
+          contains(element: T): Returns true if the set contains the specified element.
+          isEmpty(): Returns true if the set is empty.
+          size: Returns the number of elements in the set.
+
+
+EXAMPLE
+
+val immutableSet = setOf("apple", "banana", "orange")
+val containsOrange = immutableSet.contains("orange") // Check if an element exists
+
+val immutableSet = setOf(1, 2, 3)
+val containsElement = immutableSet.contains(2)
+val isEmpty = immutableSet.isEmpty()
+val size = immutableSet.size
 
 
 
 
-
-
-
-
-
-
-
+The methods and features available in each collection type allow for convenient manipulation and retrieval of data. 
+Immutable collections provide a guarantee that the data will not change, while mutable collections allow modifications to the data as needed.
+By choosing the appropriate collection type and methods, you can efficiently work with collections in Kotlin.
 
 
 
